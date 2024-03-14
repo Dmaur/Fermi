0f7688c4a573f014785d115b61bdc800540aeacc
Dmaur <Derrickmaurais1@gmail.com>
2024-03-14 19:53:39 -0300

    got the gui set up

--- src/FermiWindow.java
+++ src/FermiWindow.java
@@ -1,9 +1,9 @@
 import java.awt.Dimension;
 
 import javax.swing.*;
-// import javax.swing.border.EmptyBorder;
-// import java.awt.*;
-// import java.awt.event.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.*;
 import net.miginfocom.swing.MigLayout;
 
 
@@ -20,7 +20,7 @@ public class FermiWindow extends JFrame {
     public FermiWindow(){
         // ----------window constuctor
         this.setTitle("Fermi Guessing Game");
-        this.setSize(550,550);  
+        this.setSize(550,400);  
         this.setLocation(600,200);
         this.setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,22 +29,25 @@ public class FermiWindow extends JFrame {
         mainPanel = new JPanel( );
         this.add(mainPanel);
         // ----------------------mig layout for main panel
-        mainPanel.setLayout(new MigLayout("debug",
-                                              "[275][275]",
+        mainPanel.setLayout(new MigLayout("",
+                                              "[250][250]",
                                               "[525]"));
-
+        // setting up the left pannel with the buttons and text fields. 
         leftPanel = new JPanel();
-        JLabel enterMsg = new JLabel("Enter your guesses (0-9)");
-        numOne = new JTextField(5);
-        numTwo = new JTextField(5);
-        numThree = new JTextField(5);
+        JLabel enterMsg = new JLabel("Enter your three guesses (0-9)");
+        numOne = new JTextField(8);
+        numTwo = new JTextField(8);
+        numThree = new JTextField(8);
         JButton okay = new JButton("OK");
+        okay.setPreferredSize(new Dimension(100,0));
         JButton reset = new JButton("reset");
+        reset.setPreferredSize(new Dimension(100,0));
         leftPanel.setSize(200,400);
+        // mig layout for left panel. 
         leftPanel.setLayout(new MigLayout("",
                                                 "[240]",
-                                                "30[]10[]10[]10[]10[30]280[30]"));
-
+                                                "30[]10[]10[]10[]10[30]140[30]"));
+        //adding all the contents to the left panel.  
         leftPanel.add(enterMsg,"wrap");
         leftPanel.add(numOne, "wrap");
         leftPanel.add(numTwo,"wrap");
@@ -52,8 +55,20 @@ public class FermiWindow extends JFrame {
         leftPanel.add(okay,"wrap");
         leftPanel.add(reset,"wrap");
 
-
+        // setting up the right panel with text area and lable. 
         rightPanel = new JPanel();
+        JLabel hints = new JLabel("Hints:");
+        JTextArea gameOutput = new JTextArea();
+        Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+        gameOutput.setPreferredSize(new Dimension(240,280));
+        rightPanel.setSize(200,400);
+       
+        // setting mig layout
+        rightPanel.setLayout(new MigLayout("","[]","[]10[]120"));
+        // adding contents to the rightPanel. 
+        rightPanel.add(hints,"wrap");
+        rightPanel.add(gameOutput);
+        gameOutput.setBorder(border);
 
 
         mainPanel.add(leftPanel);
